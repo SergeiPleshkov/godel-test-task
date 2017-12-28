7ac156acca27aa51c7df5b11eca5595811204d53
Sergei Pleshkov <azrailomgpro@gmail.com>
2017-12-28 22:15:07 +0300

    v1.1. Added some css, added game timer

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 'use strict';
 
 let randomized = [null, 1, 2, 3, 4, 5, 6, 7, 8].sort(() => 0.5 - Math.random());
-var gameState = new Array(3);
+let gameState = new Array(3);
 
-for (var i = 0; i < 3; i++) {
+for (let i = 0; i < 3; i++) {
     gameState[i] = new Array(3);
-    for (var j = 0; j < 3; j++) {
+    for (let j = 0; j < 3; j++) {
         gameState[i][j] = randomized[i * 3 + j]
     }
 }
-
-console.log(gameState[1-1][2])
+gameState = [[null, 1, 2], [3, 4, 5], [6, 7, 8]]
 
 function renderTable(state) {
+    document.querySelector('.content').innerHTML = '';
     let table = '<table>';
     let newState = gameState.slice();
-    for (var i = 0; i < 3; i++) {
+    for (let i = 0; i < 3; i++) {
         table += '<tr>';
-        for (var j = 0; j < 3; j++) {
+        for (let j = 0; j < 3; j++) {
             let thisCell = state[i][j];
             table += `<td id="${(i||0)+''+(j||0)}" class="${!thisCell?'empty':'filled'}">${thisCell || ''}</td>`;
             newState[i][j] = thisCell;
@@ -29,10 +29,10 @@ function renderTable(state) {
     document.querySelector('.content').innerHTML += table;
     gameState = newState.slice()
 }
-
-renderTable(gameState);
-
-console.log(gameState)
+if (confirm('Начать игру?')) {
+    renderTable(gameState);
+}
+let start = Date.now();
 
 function moveCell(cell) {
     let emptyCell = document.querySelector('.empty'),
@@ -40,20 +40,22 @@ function moveCell(cell) {
         thisCol = cell.id.slice(-1),
         emptyRow = emptyCell.id.slice(0, 1),
         emptyCol = emptyCell.id.slice(-1);
-
-    debugger;
     
     if (((Math.abs(thisCol - emptyCol) === 1) && (thisRow === emptyRow)) || ((Math.abs(thisRow - emptyRow) === 1) && (thisCol === emptyCol))) {
-        let cache = gameState[thisRow][thisCol];
-        gameState[emptyRow][emptyCol] = cache;
+        gameState[emptyRow][emptyCol] = gameState[thisRow][thisCol];
         gameState[thisRow][thisCol] = null;
-        document.querySelector('.content').innerHTML = '';
         renderTable(gameState);
-    }
-}
+}}
 
-document.addEventListener('click', (ev, gameState) => {
+document.addEventListener('click', (ev) => {
     if (ev.target.tagName == 'TD') {
-        moveCell(ev.target, gameState);
+        moveCell(ev.target);
+    }
+    
+    if (JSON.stringify(gameState) === JSON.stringify([[null, 1, 2], [3, 4, 5], [6, 7, 8]])) {
+        let end = Date.now();
+        let playedSec = Math.round((end - start)/1000);
+        let playedMin = Math.round(playedSec/60);
+        setTimeout(() => confirm(`Ура! Вы решили головоломку за ${playedMin}:${playedSec % 60 < 10? '0'+ playedSec % 60 : playedSec % 60}`), 0)
     }
 })
