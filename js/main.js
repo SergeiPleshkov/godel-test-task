1f12a313797ac87079fd1d3e3444a306ecc3414c
Sergei Pleshkov <azrailomgpro@gmail.com>
2017-12-29 00:50:19 +0300

    v1.2. Added start screen with field options. Added css, refactored js.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,50 @@
 'use strict';
 
-let randomized = [null, 1, 2, 3, 4, 5, 6, 7, 8].sort(() => 0.5 - Math.random());
-let gameState = new Array(3);
+let gameState,
+    solvedState,
+    start;
+renderStartScreen();
 
-for (let i = 0; i < 3; i++) {
-    gameState[i] = new Array(3);
-    for (let j = 0; j < 3; j++) {
-        gameState[i][j] = randomized[i * 3 + j]
+function createInitialState(rows, cols) {
+
+    let arr = [null];
+
+    for (let i = 1; i < cols * rows; i++) {
+        arr.push(i);
     }
+
+    solvedState = new Array(rows);
+
+    for (let i = 0; i < rows; i++) {
+        solvedState[i] = new Array(cols);
+        for (let j = 0; j < cols; j++) {
+            solvedState[i][j] = arr[i * cols + j];
+        }
+    }
+
+    let randomized = arr.sort(() => Math.random() - 0.5);
+
+    gameState = new Array(rows);
+    console.log(gameState)
+
+    for (let i = 0; i < rows; i++) {
+        gameState[i] = new Array(cols);
+        for (let j = 0; j < cols; j++) {
+            gameState[i][j] = randomized[i * cols + j]
+        }
+    }
+    console.log(gameState, arr, randomized);
 }
-gameState = [[null, 1, 2], [3, 4, 5], [6, 7, 8]]
 
 function renderTable(state) {
     document.querySelector('.content').innerHTML = '';
     let table = '<table>';
     let newState = gameState.slice();
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < gameState.length; i++) {
         table += '<tr>';
-        for (let j = 0; j < 3; j++) {
+        for (let j = 0; j < gameState[0].length; j++) {
             let thisCell = state[i][j];
-            table += `<td id="${(i||0)+''+(j||0)}" class="${!thisCell?'empty':'filled'}">${thisCell || ''}</td>`;
+            table += `<td id="${i + '' + j}" class="${!thisCell ? 'empty' : 'filled'}">${thisCell || ''}</td>`;
             newState[i][j] = thisCell;
         }
         table += '</tr>';
@@ -29,10 +54,15 @@ function renderTable(state) {
     document.querySelector('.content').innerHTML += table;
     gameState = newState.slice()
 }
-if (confirm('Начать игру?')) {
-    renderTable(gameState);
+
+function renderStartScreen() {
+    document.querySelector('.content').innerHTML = `
+    <div class="startScreen">
+        <input type="number" min="3" max="10" class="rows" id="rows" value="3"><span>Высота</span>
+        <input type="number" min="3" max="10" class="cols" id="cols" value="3"><span>Ширина</span>
+        <button class="startGameBtn">Начать игру</button>
+    </div>`
 }
-let start = Date.now();
 
 function moveCell(cell) {
     let emptyCell = document.querySelector('.empty'),
@@ -40,22 +70,33 @@ function moveCell(cell) {
         thisCol = cell.id.slice(-1),
         emptyRow = emptyCell.id.slice(0, 1),
         emptyCol = emptyCell.id.slice(-1);
-    
+
     if (((Math.abs(thisCol - emptyCol) === 1) && (thisRow === emptyRow)) || ((Math.abs(thisRow - emptyRow) === 1) && (thisCol === emptyCol))) {
         gameState[emptyRow][emptyCol] = gameState[thisRow][thisCol];
         gameState[thisRow][thisCol] = null;
         renderTable(gameState);
-}}
+    }
+}
 
 document.addEventListener('click', (ev) => {
     if (ev.target.tagName == 'TD') {
         moveCell(ev.target);
+        if (JSON.stringify(gameState) === JSON.stringify(solvedState)) {
+            let end = Date.now();
+            let playedSec = Math.round((end - start) / 1000);
+            let playedMin = Math.round(playedSec / 60);
+            setTimeout(() => {
+                if (confirm(`Ура! Вы решили головоломку за ${playedMin}:${playedSec % 60 < 10? '0'+ playedSec % 60 : playedSec % 60}!
+
+                        Сыграете ещё раз?`)) {
+                    renderStartScreen();
+                };
+            }, 0);
+        }
     }
-    
-    if (JSON.stringify(gameState) === JSON.stringify([[null, 1, 2], [3, 4, 5], [6, 7, 8]])) {
-        let end = Date.now();
-        let playedSec = Math.round((end - start)/1000);
-        let playedMin = Math.round(playedSec/60);
-        setTimeout(() => confirm(`Ура! Вы решили головоломку за ${playedMin}:${playedSec % 60 < 10? '0'+ playedSec % 60 : playedSec % 60}`), 0)
+    if (ev.target.className == 'startGameBtn') {
+        createInitialState(document.querySelector('.rows').value, document.querySelector('.cols').value);
+        renderTable(gameState);
+        start = Date.now();
     }
 })
