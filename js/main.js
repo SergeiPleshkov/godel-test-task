d55463670f34614f50a8222375baff0e30bb2699
Sergei Pleshkov <azrailomgpro@gmail.com>
2017-12-28 17:54:34 +0300

    added better randomizing, move-on-click

--- js/main.js
+++ js/main.js
@@ -1,26 +1,6 @@
 'use strict';
 
-let gameState = [null, 1, 2, 3, 4, 5, 6, 7, 8];
-
-function shuffle(array) {
-    var currentIndex = array.length,
-        temporaryValue, randomIndex;
-
-    while (0 !== currentIndex) {
-
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
-
-let randomizedNums = shuffle(gameState).slice();
-console.log(randomizedNums);
+let gameState = [null, 1, 2, 3, 4, 5, 6, 7, 8].sort(() => 0.5 - Math.random());
 
 function renderRandomTable(state) {
     let table = '<table>';
@@ -29,7 +9,7 @@ function renderRandomTable(state) {
         table += '<tr>';
         for (var j = 0; j < 3; j++) {
             let thisCell = state.shift(1);
-            table += `<td class="cell-${newState.length}">${thisCell || ''}</td>`;
+            table += `<td class="${newState.length} ${!thisCell?'empty':'filled'}">${thisCell || ''}</td>`;
             newState.push(thisCell) - 1;
         }
         table += '</tr>';
@@ -40,16 +20,27 @@ function renderRandomTable(state) {
     gameState = newState.slice()
 }
 
-renderRandomTable(randomizedNums);
+renderRandomTable(gameState);
 
 function moveCell(cell) {
+    let emptyCell = document.querySelector('.empty');
     let cellClass = cell.className;
-    let cellNum =  cellClass.slice(-1);
-    console.log(cellNum)
+    let cellNum = cellClass.slice(0, 1);
+    
+    let emptyCellClass = emptyCell.className;
+    let emptyCellNum = emptyCellClass.slice(0, 1);
+    if (emptyCellNum == cellNum - 1 || emptyCellNum == cellNum + 1 || emptyCellNum == cellNum - 3 || emptyCellNum == cellNum + 3) {
+        emptyCell.innerHTML = cell.innerHTML;
+        emptyCell.classList.toggle('empty');
+        emptyCell.classList.toggle('filled');
+        cell.classList.toggle('filled');
+        cell.classList.toggle('empty', true);
+        cell.innerHTML = '';
+    }
 }
 
 document.addEventListener('click', (ev) => {
-    if (ev.target.tagName = 'TD') {
+    if (ev.target.tagName == 'TD') {
         moveCell(ev.target);
     }
 })
