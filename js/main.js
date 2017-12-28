a98a1fe5367b2ac6521cbd7a859b410f467e26be
Sergei Pleshkov <azrailomgpro@gmail.com>
2017-12-28 21:04:58 +0300

    MVP 1.0

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
 'use strict';
 
-let gameState = [null, 1, 2, 3, 4, 5, 6, 7, 8].sort(() => 0.5 - Math.random());
+let randomized = [null, 1, 2, 3, 4, 5, 6, 7, 8].sort(() => 0.5 - Math.random());
+var gameState = new Array(3);
 
-function renderRandomTable(state) {
+for (var i = 0; i < 3; i++) {
+    gameState[i] = new Array(3);
+    for (var j = 0; j < 3; j++) {
+        gameState[i][j] = randomized[i * 3 + j]
+    }
+}
+
+console.log(gameState[1-1][2])
+
+function renderTable(state) {
     let table = '<table>';
-    let newState = [];
+    let newState = gameState.slice();
     for (var i = 0; i < 3; i++) {
         table += '<tr>';
         for (var j = 0; j < 3; j++) {
-            let thisCell = state.shift(1);
-            table += `<td class="${newState.length} ${!thisCell?'empty':'filled'}">${thisCell || ''}</td>`;
-            newState.push(thisCell) - 1;
+            let thisCell = state[i][j];
+            table += `<td id="${(i||0)+''+(j||0)}" class="${!thisCell?'empty':'filled'}">${thisCell || ''}</td>`;
+            newState[i][j] = thisCell;
         }
         table += '</tr>';
     }
@@ -20,27 +30,30 @@ function renderRandomTable(state) {
     gameState = newState.slice()
 }
 
-renderRandomTable(gameState);
+renderTable(gameState);
+
+console.log(gameState)
 
 function moveCell(cell) {
-    let emptyCell = document.querySelector('.empty');
-    let cellClass = cell.className;
-    let cellNum = cellClass.slice(0, 1);
+    let emptyCell = document.querySelector('.empty'),
+        thisRow = cell.id.slice(0, 1),
+        thisCol = cell.id.slice(-1),
+        emptyRow = emptyCell.id.slice(0, 1),
+        emptyCol = emptyCell.id.slice(-1);
+
+    debugger;
     
-    let emptyCellClass = emptyCell.className;
-    let emptyCellNum = emptyCellClass.slice(0, 1);
-    if (emptyCellNum == cellNum - 1 || emptyCellNum == cellNum + 1 || emptyCellNum == cellNum - 3 || emptyCellNum == cellNum + 3) {
-        emptyCell.innerHTML = cell.innerHTML;
-        emptyCell.classList.toggle('empty');
-        emptyCell.classList.toggle('filled');
-        cell.classList.toggle('filled');
-        cell.classList.toggle('empty', true);
-        cell.innerHTML = '';
+    if (((Math.abs(thisCol - emptyCol) === 1) && (thisRow === emptyRow)) || ((Math.abs(thisRow - emptyRow) === 1) && (thisCol === emptyCol))) {
+        let cache = gameState[thisRow][thisCol];
+        gameState[emptyRow][emptyCol] = cache;
+        gameState[thisRow][thisCol] = null;
+        document.querySelector('.content').innerHTML = '';
+        renderTable(gameState);
     }
 }
 
-document.addEventListener('click', (ev) => {
+document.addEventListener('click', (ev, gameState) => {
     if (ev.target.tagName == 'TD') {
-        moveCell(ev.target);
+        moveCell(ev.target, gameState);
     }
 })
